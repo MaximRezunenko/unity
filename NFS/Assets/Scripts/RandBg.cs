8bd35978766b377f30abb13cdde6edb55d38e606
Max Rezunenko <maximalist1998@gmail.com>
2017-07-15 17:30:15 +0300

    add nfs

---
+++ NFS/Assets/Scripts/RandBg.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RandBg : MonoBehaviour {
+    public Material[] materials;
+	
+	void Start () {
+        GetComponent<Skybox>().material = materials[ Random.Range (0, materials.Length )];
+	
+	}
+	
+}
